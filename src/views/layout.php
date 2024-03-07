bd3d93638c074ccc0c12fad57afc1322f282e908
Maxime-L-G <maxime.lecourt457@gmail.com>
2024-03-07 14:53:25 +0100

    all computer

--- src/views/layout.php
+++ src/views/layout.php
@@ -4,15 +4,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../assets/css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <title>Computer Concept</title>
 </head>
 <body>
 <header>
-<nav class="navbar bg-body-tertiary">
-  <!-- <div class="container"> -->
-        <img class="logo" src="../assets/img/logo.avif" alt="Logo">
-  <!-- </div> -->
-  </nav>
+<nav class="navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/img/logo.png" alt="Logo" width="80" height="50" class="d-inline-block align-text-top">
+    </a>
+  </div>
+</nav>
 </header>
 <main>
   <?php require '../src/controllers/' . $route . '_controller.php'; ?>
--- src/views/personnal_computer.php
+++ src/views/personnal_computer.php
@@ -1,4 +1,13 @@
-
+<div class="container">
+  <div class="card" style="width: 20rem;">
+    <img src="../assets/img/pc.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Pc Gamer</h5>
+      <p class="card-text">Pc pour tout gamer qui aime tryhard les jeux en ultra</p>
+      <a href="#" class="btn btn-primary">Voir la config</a>
+    </div>
+  </div>
+</div>
 
 
 
